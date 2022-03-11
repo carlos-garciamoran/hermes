f21316661d84a414bd8c1abfcfdda9c4882e148f
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2022-03-10 18:17:25 -0800

    implement Telegram alerts; clean code

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/adshao/go-binance/v2 v2.3.4 // indirect
 	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.13.1 // indirect
 	github.com/markcheno/go-talib v0.0.0-20190307022042-cd53a9264d70 // indirect
--- main.go
+++ main.go
@@ -12,19 +12,28 @@ import (
 
 	"github.com/adshao/go-binance/v2"
 	"github.com/adshao/go-binance/v2/futures"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 	"github.com/markcheno/go-talib"
 	"github.com/rs/zerolog"
 )
 
+const ID int64 = 445996511
 const INTERVAL string = "1m"
 const LIMIT int = 200
 
+var bot *tgbotapi.BotAPI
 var symbolCloses = make(map[string][]float64) // {"BTCUSDT": [40004.75, ...], ...}
 
+var emoji = map[string]string{
+	"bullish": "🐗",
+	"bearish": "🐻",
+}
+
 func initLogging() zerolog.Logger {
-	zerolog.TimeFieldFormat = time.RFC3339 // time.RFC3339Nano, time.RFC822, zerolog.TimeFormatUnix
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	zerolog.TimeFieldFormat = time.RFC3339Nano // time.RFC3339, time.RFC822, zerolog.TimeFormatUnix
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339Nano}
+
 	output.FormatLevel = func(i interface{}) string {
 		return strings.ToUpper(fmt.Sprintf("| %-5s|", i))
 	}
@@ -32,14 +41,14 @@ func initLogging() zerolog.Logger {
 	return zerolog.New(output).With().Timestamp().Logger()
 }
 
-func loadEnvFile() (string, string) {
+func loadEnvFile() (string, string, string) {
 	err := godotenv.Load()
 
 	if err != nil {
 		panic("Error loading .env file")
 	}
 
-	return os.Getenv("BINANCE_APIKEY"), os.Getenv("BINANCE_SECRETKEY")
+	return os.Getenv("BINANCE_APIKEY"), os.Getenv("BINANCE_SECRETKEY"), os.Getenv("TELEGRAM_APITOKEN")
 }
 
 func fetchSymbols(wg sync.WaitGroup, futuresClient *futures.Client) map[string]string {
@@ -55,8 +64,6 @@ func fetchSymbols(wg sync.WaitGroup, futuresClient *futures.Client) map[string]s
 		if asset.QuoteAsset == "USDT" && asset.ContractType == "PERPETUAL" && asset.UnderlyingType == "COIN" && asset.Status == "TRADING" {
 			symbol := asset.Symbol
 			symbolIntervalPair[symbol] = INTERVAL
-
-			fmt.Println("💡 Caching", symbol)
 
 			wg.Add(1)
 
@@ -75,7 +82,6 @@ func fetchInitialCloses(symbol string, wg sync.WaitGroup, futuresClient *futures
 		Do(context.Background())
 
 	if err != nil {
-		fmt.Println("HERE")
 		panic(err)
 	}
 
@@ -88,33 +94,86 @@ func fetchInitialCloses(symbol string, wg sync.WaitGroup, futuresClient *futures
 		}
 	}
 
-	fmt.Printf("✅ %-12s fetched %d candles\n", symbol, kline_count)
+	fmt.Printf("💡 %-12s: downloaded %d candles\n", symbol, kline_count)
+}
+
+func calculate_EMA_cross(ema_9 []float64, ema_21 []float64) string {
+	var delta [3]int
+	var sum int
+
+	for i := 0; i < 3; i++ {
+		if ema_9[i] < ema_21[i] {
+			delta[i] = -1
+		} else {
+			delta[i] = 1
+		}
+	}
+
+	for _, v := range delta {
+		sum += v
+	}
+
+	// If all deltas are the same (3 or -3), there can be no cross.
+	if sum%3 != 0 {
+		// Check the cross on the last candle.
+		if delta[2] == 1 {
+			return "bullish"
+		} else if delta[2] == -1 {
+			return "bearish"
+		}
+	}
+
+	return "none"
+}
+
+func sendTelegramAlert(bot *tgbotapi.BotAPI, symbol string, side string, RSI float64) {
+	text := fmt.Sprintf("%s *%s*: %s cross ⚡️\n"+
+		"    - RSI: %.2f",
+		emoji[side], symbol[:len(symbol)-4], side, RSI,
+	)
+
+	msg := tgbotapi.MessageConfig{
+		BaseChat: tgbotapi.BaseChat{
+			ChatID: ID,
+		},
+		Text:      text,
+		ParseMode: tgbotapi.ModeMarkdown,
+	}
+
+	if _, err := bot.Send(msg); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	// Used for building initial candles cache async & quickly
+	var err error
 	var wg sync.WaitGroup
 
 	log := initLogging()
 
-	apiKey, secretKey := loadEnvFile()
+	apiKey, secretKey, telegramToken := loadEnvFile()
+
+	bot, err = tgbotapi.NewBotAPI(telegramToken)
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+
+	// NOTE: may want to change on prod
+	bot.Debug = true
 
 	futuresClient := binance.NewFuturesClient(apiKey, secretKey)
 
 	symbols := fetchSymbols(wg, futuresClient)
 
-	log.Info().
-		Int("count", len(symbols)).
-		Msg("🪙 Fetched symbols")
-
+	// Handle CTRL-C (may want to print something)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
-	// Handle CTRL-C (may want to print something)
 	go func() {
 		for sig := range c {
 			fmt.Println(sig)
-			os.Exit(0)
+			os.Exit(1)
 		}
 	}()
 
@@ -157,19 +216,24 @@ func main() {
 
 		symbolCloses[symbol] = closes // Update the [global] map
 
-		rsi := talib.Rsi(closes, 14)
-		ema_9, ema_21 := talib.Ema(closes, 9), talib.Ema(closes, 21)
+		rsi := talib.Rsi(closes, 14)[lastCloseIndex]
+		ema_9, ema_21 := talib.Ema(closes, 9)[lastCloseIndex-2:], talib.Ema(closes, 21)[lastCloseIndex-2:]
+
+		// TODO: store in Order object
+		EMA_cross := calculate_EMA_cross(ema_9, ema_21)
+
+		if EMA_cross != "none" {
+			sendTelegramAlert(bot, symbol, EMA_cross, rsi)
+		}
 
 		log.Info().
 			// Int("StartTime", int(k.StartTime/10000)).
-			// Float64("Open", parsedCandle["Open"]).
-			// Float64("High", parsedCandle["High"]).
-			// Float64("Low", parsedCandle["Low"]).
 			Float64("Close", parsedCandle["Close"]).
-			Float64("RSI", rsi[lastCloseIndex]).
-			Float64("EMA_09", ema_9[lastCloseIndex]).
-			Float64("EMA_21", ema_21[lastCloseIndex]).
-			Msg(symbol)
+			Float64("EMA_09", ema_9[2]).
+			Float64("EMA_21", ema_21[2]).
+			Str("EMA_cross", EMA_cross).
+			Float64("RSI", rsi).
+			Msg(symbol[:len(symbol)-4]) // No need to print "USDT"
 	}
 
 	errHandler := func(err error) { log.Fatal().Msg(err.Error()) }
@@ -179,13 +243,15 @@ func main() {
 	// TODO: find better way to wait for the cache to be built before starting the WS
 	time.Sleep(2 * time.Second)
 
-	doneC, _, err := futures.WsCombinedKlineServe(symbols, wsKlineHandler, errHandler)
+	log.Info().Int("count", len(symbols)).Msg("🪙 Fetched symbols")
 
-	log.Debug().Msg("🔌 Socket initialised")
+	doneC, _, err := futures.WsCombinedKlineServe(symbols, wsKlineHandler, errHandler)
 
 	if err != nil {
 		log.Fatal().Msg(err.Error())
 	}
+
+	log.Debug().Msg("🔌 Socket initialised")
 
 	<-doneC
 }
